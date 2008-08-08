8c70bbb646291651baef7e9517af2984bd0989ca
danillonunes <danillonunes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-08 20:38:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/login-box/tags/latest@58740 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- login-box.php
+++ login-box.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Login-box
 Plugin URI: http://danillonunes.net/en/wordpress/login-box
-Version: 2.0.1
+Version: 2.0.2
 Description: Inserts in all pages a hidden login box, that you can open pressing a combination of keys (like Ctrl + E) 
 Author: Marcus Danillo
 Author URI: http://danillonunes.net/en
